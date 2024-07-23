73de5691e1ba4f4bd090f0a788a4dfa3b7598ba5
Aryankashyap266 <Aryankashyap266@gmail.com>
2024-07-23 23:04:10 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,11 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
-app.use(express.static)
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
 
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 
